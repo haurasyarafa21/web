130394b293493c59c3bfae4001600606c5c838df
Haura Syarafa <haura.syarafa21@ui.ac.id>
2015-09-07 14:21:53 +0000

    new1

--- functional_tests.py
+++ functional_tests.py
@@ -7,8 +7,8 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+	#def tearDown(self):
+		#self.browser.quit()
 
 	def test_can_start_a_list_and_retreive_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		#self.fail('Finish the test!')
 
 		# She is invited to enter a to-do item straight away
 		

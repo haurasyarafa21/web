0dd195fba83cd9c2ca4f107c50d9c5f28b5b574b
Haura Syarafa <haura.syarafa21@ui.ac.id>
2015-09-11 12:48:47 +0000

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -49,6 +49,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.fail('Finish the test!')
 
 		# The page updates again, and now shows both items on her list
-		
+		# lala
+
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')

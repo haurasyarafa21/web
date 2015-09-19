31252992f8953386160f690150471f15099166fe
Haura Syarafa <haura.syarafa21@ui.ac.id>
2015-09-19 07:16:21 +0000

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,7 @@ from lists.models import Item
 def home_page(request):
 	if request.method == 'POST':
 		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/')
+		return redirect('/lists/the-only-list-in-the-world/')
 
 	komen = ''
 	if Item.objects.count()==0:
@@ -16,4 +16,8 @@ def home_page(request):
 		komen='oh tidak'	
 	
 	items = Item.objects.all()
-	return render(request, 'home.html', {'items': items, 'komen': komen})
+	return render(request, 'home.html', {'komen': komen})
+
+def view_list(request):
+	items = Item.objects.all()
+	return render(request, 'list.html', {'items': items})
